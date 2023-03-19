72300ff2ab753eecbc791c9ba06431e057ab1468
mucoz <72700035+mucoz@users.noreply.github.com>
2023-03-19 23:41:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from routes.routes import auth_blueprint, user_blueprint, \
@@ -7,6 +7,10 @@ from routes.routes import auth_blueprint, user_blueprint, \
 from database import db
 from config import Config
 from flask_mail import Mail
+from jwt.exceptions import ExpiredSignatureError
+from flask_jwt_extended.exceptions import NoAuthorizationError
+import controllers.messages.messages as msg
+
 
 app = Flask(__name__)
 CORS(app)
@@ -22,6 +26,23 @@ app.register_blueprint(attendee_blueprint, url_prefix='/api/v1/attendees')
 app.register_blueprint(expense_blueprint, url_prefix='/api/v1/expenses')
 app.register_blueprint(checkout_blueprint, url_prefix='/api/v1/checkout')
 # app.register_blueprint(sendmail_blueprint, url_prefix='/api/v1/sendmail')
+
+
+@app.errorhandler(NoAuthorizationError)
+def handle_missing_authorization_error(e):
+    return jsonify(msg.ERROR_NO_AUTHENTICATION), 401
+
+
+# Custom error handler for ExpiredSignatureError
+@app.errorhandler(ExpiredSignatureError)
+def handle_expired_signature_error(e):
+    return jsonify(msg.ERROR_TOKEN_EXPIRED), 401
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify(msg.ERROR_PAGE_NOT_FOUND), 404
+
 
 with app.app_context():
     db.create_all()
